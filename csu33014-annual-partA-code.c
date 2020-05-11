e16a46bd63ea648af641ec5a3fb5f750e4da2419
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2020-05-11 18:17:38 +0100

    Update csu33014-annual-partA-code.c

--- csu33014-annual-partA-code.c
+++ csu33014-annual-partA-code.c
@@ -128,7 +128,7 @@ void partA_vectorized3(float *restrict a, float *restrict b, int size)
   // replace the following code with vectorized code
   __m128 a4, b4;
   float zero = 0;
-  __m128 zeros = _mm_set1_ps(zeros);
+  __m128 zeros = _mm_set1_ps(zero);
   for (int i = 0; i < size; i++)
   {
     if (a[i] < 0.0)
@@ -219,3 +219,4 @@ void partA_vectorized6(float *restrict a, float *restrict b,
   }
   a[1023] = 0.0;
 }
+
