0b80bfd1c0c9982376228ce5379acb940c00ba37
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2020-05-12 17:54:26 +0100

    Update csu33014-annual-partA-code.c

--- csu33014-annual-partA-code.c
+++ csu33014-annual-partA-code.c
@@ -272,7 +272,7 @@ void partA_vectorized6(float *restrict a, float *restrict b,
     sumVector = _mm_add_ps(sumVector, j2);
     _mm_storeu_ps(&a[i], sumVector);
   }
-  for (int i = 1; i < 1023; i++)
+  for (int i = 1021; i < 1023; i++)
   {
     float sum = 0.0;
     for (int j = 0; j < 3; j++)
